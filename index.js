5f809a5c4508a30064abdf6f56d5f147a99b285e
ryandimba <ryan.dimba@student.moringaschool.com>
2022-09-01 09:02:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const options = {
+// function displayComments(comments) {
+//     commentsList.innerHTML = "";
+//     comments.forEach(displayComment)
+// }
+// function displayComment(comment) {
+//     const newComment = document.createElement('li');
+//     newComment.textContent = comment.content;
+//     commentsList.append(newComment);
+// }
+// function addNewComment() {
+//     commentsList.addEventListener('submit', addComment)
+// }
+// function addComment(event) {
+//     event.preventDefault();
+//     const newPostedComment = event.target.comment.value;
+//     displayComment({ content: newPostedComment });
+//     event.target.reset();
+// }
+function getAllTrains(){const options = {
 	method: 'GET',
 	headers: {
+		'content-type': 'application/json',
 		'X-RapidAPI-Key': '25abe8c41cmsh2c11a550467a1f1p19fa9ejsn3cca9fa6752c',
-		'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
-	}
+		'X-RapidAPI-Host': 'Estates.p.rapidapi.com'
+	},
+	body: '{"search":"Estate"}'
 };
 
 fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+};
