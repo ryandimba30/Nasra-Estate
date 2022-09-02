b2a98826ff8f8c025f84717028ff73370833c794
ryandimba <ryan.dimba@student.moringaschool.com>
2022-09-02 13:22:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -16,18 +16,15 @@
 //     displayComment({ content: newPostedComment });
 //     event.target.reset();
 // }
-function getAllTrains(){const options = {
+const options = {
 	method: 'GET',
 	headers: {
-		'content-type': 'application/json',
 		'X-RapidAPI-Key': '25abe8c41cmsh2c11a550467a1f1p19fa9ejsn3cca9fa6752c',
-		'X-RapidAPI-Host': 'Estates.p.rapidapi.com'
-	},
-	body: '{"search":"Estate"}'
+		'X-RapidAPI-Host': 'real-estate-usa.p.rapidapi.com'
+	}
 };
 
-fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true', options)
+fetch('https://real-estate-usa.p.rapidapi.com/api/v1/properties?postal_code=94105&offset=0&limit=200', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
-};
